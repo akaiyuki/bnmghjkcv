2d44321538f37573d1b5552b81a8ef21730b754b
Trisha Corpuz <trish@smartwave.ph>
2016-09-09 11:29:16 +0800

    added fabric

--- BenzAndroid/app/src/main/java/com/av/benzandroid/functions/core/AppController.java
+++ BenzAndroid/app/src/main/java/com/av/benzandroid/functions/core/AppController.java
@@ -1,7 +1,37 @@
 package com.av.benzandroid.functions.core;
 
+import android.app.Application;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.av.benzandroid.BuildConfig;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
 /**
  * Created by CodeSyaona on 9/5/16.
  */
-public class AppController {
+public class AppController extends Application {
+
+    private static AppController mInstance;
+
+    public static synchronized AppController getInstance() {
+        return mInstance;
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (!BuildConfig.DEBUG){
+            Fabric.with(this, new Crashlytics());
+        }
+
+
+    }
+
+
+
+
 }
